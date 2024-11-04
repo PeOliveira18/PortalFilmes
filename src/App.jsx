fd7b923701ef4986c88e656fbf42ddfe057e080e
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-11-04 19:47:52 -0300

    Mudancas na pagina home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
+import styled from "styled-components"
+
+const AppContainer = styled.div`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x:hidden;
+`
 
 function App() {
 
   return (
-    <>
+    <AppContainer>
       <Header />
       <Outlet />
-    </>
+    </AppContainer>
   )
 }
 
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,13 +1,30 @@
-import left from "../images/left.svg"
-import right from "../images/right.svg"
+import { useRef } from "react";
+import left from "../images/left.svg";
+import right from "../images/right.svg";
 
 export default function CardContainer({ titulo, children }) {
+    const carrosel = useRef(null)
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carrosel.current.scrollLeft -= carrosel.current.offsetWidth;
+    };
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carrosel.current.scrollLeft += carrosel.current.offsetWidth;
+    };
+
     return (
-        <div className="max-w-full px-5 mt-5">
-            <h1>{titulo}</h1>
-            <div className="flex">
-                {children}
+        <>
+            <div className="max-w-full h-96 w-auto px-5 relative">
+                <h1 className="mt-10">{titulo}</h1>
+                <div className="flex scrollbar-hide overflow-hidden" ref={carrosel}>
+                    {children}
+                </div>
+                <button onClick={handleRightClick} className="botoes-carrosel right-0"><img src={right} alt="Right" /></button>
+                <button onClick={handleLeftClick} className="botoes-carrosel left-0"><img src={left} alt="Left"/></button>
             </div>
-        </div>
+        </>
     )
 }

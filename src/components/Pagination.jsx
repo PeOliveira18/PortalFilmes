782c5dfe370a64f7f9190d88f282628867bb7a9d
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-11-27 10:48:33 -0300

    Mudancas na paginaton

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,6 +2,25 @@ import { useState } from "react"
 
 function Pagination({setPages, pages}) {
     const [selected, setSelected] = useState(pages)
+    const [offset, setOffset] = useState(0)
+
+    const handleClickNextPage = () => {
+        const newOffset = offset + 10
+        const newPage = newOffset + 1
+        setOffset(newOffset)
+        setSelected(newPage)
+        setPages(newPage)
+    }
+
+    const handleClickPreviousPage = () => {
+        if (offset > 0) {
+            const newOffset = offset - 10
+            const newPage = newOffset + 1
+            setOffset(newOffset)
+            setSelected(newPage)
+            setPages(newPage)
+        }
+    }
     
     const handleClickPage = (page) => {
         setSelected(page)
@@ -20,6 +39,7 @@ function Pagination({setPages, pages}) {
         const newPage = pages + 1
         setPages(newPage)
         setSelected(newPage)
+        
     }
 
 
@@ -28,29 +48,41 @@ function Pagination({setPages, pages}) {
         <nav aria-label="Page navigation example" className="my-10">
             <ul class="flex items-center -space-x-px h-10 text-base">
                 <li>
-                    <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickPrevious}>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickPreviousPage}>
+                        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
                         <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                         </svg>
                     </a>
                 </li>
                 <li>
-                    <a href="#" className={`btn-categorias ${selected === 1 ? 'btn-ativo' : ''}`} onClick={() => handleClickPage(1)}>1</a>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickPrevious}>
+                        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                    </a>
+                </li>
+                    {[...Array(10)].map((_, index) => {
+                        const page = index + 1 + offset;
+                        return (<li>
+                            <a href="#" className={`btn-categorias ${selected === page ? "btn-ativo" : ""}`} onClick={() => handleClickPage(page)}>{page}</a>
+                        </li>
+                        )
+                    })}
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickNext}>
+                        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" className={`btn-categorias ${selected === 2 ? 'btn-ativo' : ''}`} onClick={() => handleClickPage(2)}>2</a>
-                </li>
-                <li>
-                    <a href="#" className={`btn-categorias ${selected === 3 ? 'btn-ativo' : ''}`} onClick={() => handleClickPage(3)}>3</a>
-                </li>
-                <li>
-                    <a href="#" className={`btn-categorias ${selected === 4 ? 'btn-ativo' : ''}`} onClick={() => handleClickPage(4)}>4</a>
-                </li>
-                <li>
-                    <a href="#" className={`btn-categorias ${selected === 5 ? 'btn-ativo' : ''}`} onClick={() => handleClickPage(5)}>5</a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickNext}>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={handleClickNextPage}>
+                        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
                         <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                         </svg>
